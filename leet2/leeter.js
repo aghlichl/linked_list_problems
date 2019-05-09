53031d1cd98dd1b2265feefe2a3c8988e3c21456
Ari <aurashghlichloo@gmail.com>
2019-05-08 23:33:57 -0700

    attempted answer

--- leet2/leeter.js
+++ leet2/leeter.js
@@ -3,5 +3,14 @@
  * @return {number}
  */
 var reverse = function (x) {
+    let result = []
+    while (x > 10) {
+        let remainder = x % 10;
+        console.log(remainder)
+        result.push(remainder);
+        x = (x - remainder) / 10;
+    }
+    result.push(x);
 
+    return result.join('');
 };
