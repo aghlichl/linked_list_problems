66de99f86ed7fb8808d695f6adb2a960dc44e2ad
Ari <aurashghlichloo@gmail.com>
2019-05-13 23:24:33 -0700

    mock interview practice problem

--- leet2/leeter.js
+++ leet2/leeter.js
@@ -1,17 +1,17 @@
-/**
- * @param {number} x
- * @return {number}
- */
-var reverse = function (x) {
-    let result = []
-    result.push(x);
-    while (x > 10) {
-        let remainder = x % 10;
-        console.log(remainder)
-        result.push(remainder);
-        x = (x - remainder) / 10;
+//root node, val left and right
+/*
+
+
+*/
+
+
+
+function traversal(node){
+    if(!node){
+        return [];
     }
     
+    let result = []
+    return traversal(node.left) + [node.val] + traversal(node.right);
 
-    return result.join('');
-};
+}
